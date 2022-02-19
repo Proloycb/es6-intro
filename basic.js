6250d77dfb4c0c4d45cfd604b38fbaaa3d34764c
Proloycb <proloychacroborty@gmail.com>
2022-02-19 12:43:10 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,53 @@
+// variable
+var deposit = 400 ;
+if(deposit < 500){
+    
+}
+else if(deposit > 500){
+
+}
+else if(deposit == 500){
+
+}
+else if(deposit != 500){
+
+}
+else if(deposit <= 500){
+
+}
+else if(deposit >= 500){
+
+}
+else{
+
+}
+// array
+const numbers = [23, 333, 245, 678, 12, 231];
+const numberCount = numbers.length ;
+numbers.push(111);
+numbers.pop();
+numbers[2] = 444 ;
+// check whether included array
+if(numbers.indexOf(222) != -1){
+
+}
+if(numbers.includes(222)){
+
+}
+
+// loop
+// while, for
+for(const number of numbers){
+
+}
+
+// function
+function fullName (firstName, lastName){
+    const name = firstName + ' ' + lastName ;
+    return name;
+}
+const person = fullName('Bonna', 'CB');
+// console.log(person);
+
+// object 
+const bottle = {color: 'white', contain: 'oil'};
