0691c4ca26b28ed254f3bb187223bd603f9efd45
Proloycb <proloychacroborty@gmail.com>
2022-02-19 13:25:54 +0600

    finished

---
+++ let-const.js
@@ -0,0 +1,20 @@
+let balance = 1240;
+balance = 1340;
+console.log(balance);
+
+const userName = 'Janpakhi potas potas';
+const weTogether = 'ami' + userName ;
+console.log(weTogether);
+// userName = 'moyna pakhi';
+
+const numbers = [23,45,66,77,88];
+numbers.push(22);
+numbers[2] = 33;
+for(let i = 0; i < numbers.length; i++){
+    const number = numbers[i];
+    console.log(number);
+}
+
+const products = {name: 'phone', price: '10000', brand: 'tecno'};
+products.brand ='iphone';
+// products = {}
